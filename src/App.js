12050e7fd39b2f1a49437a49abebbbe26371b1c8
kaya.ryu <kaya.ryu@asurion.com>
2023-02-11 16:39:18 +0900

    3-3 planning the component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
-  // Mounting: constructor >> JS thing, not a big deal
-  // constructor(props) {
-  //   super(props);
-  //   console.log("hello");
-  // }
   state = {
-    count: 0,
+    isLoading: true,
+    movie: [],
   };
-  add = () => {
-    // console.log("add");
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    // console.log("minus");
-    this.setState({ count: this.state.count - 1 });
-  };
-
-  counponentDidMount() {
-    console.log("didmount");
-  }
-  componentDidUpdate() {
-    console.log("didUpdate");
-  }
-  componentWillUnmount() {
-    // when component goes away
-    console.log("willunmount");
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 1000);
   }
 
   render() {
-    console.log("render");
-    return (
-      <div>
-        <h1>The number: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "Ready"}</div>;
   }
 }
 
