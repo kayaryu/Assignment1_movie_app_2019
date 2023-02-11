cf6139ad771de6aec18e40ea11eef9d0d4f83b7f
kaya.ryu <kaya.ryu@asurion.com>
2023-02-11 15:38:54 +0900

    2-2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React from "react";
 
-function Food({ favorite }) {
-  return <h1>I like {favorite} </h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img
+        src={picture}
+        width="240"
+      />
+    </div>
+  );
 }
+
+const foodIlike = [
+  {
+    name: "wine",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+  },
+  {
+    name: "potato",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+  },
+  {
+    name: "tomato",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+  },
+  {
+    name: "sth",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food favorite="sth" />
-      <Food favorite="ramen" />
-      <Food favorite="sangria" />
-      <Food favorite="wine" />
+      {foodIlike.map((dish) => (
+        <Food
+          name={dish.name}
+          picture={dish.image}
+        />
+      ))}
     </div>
   );
 }
