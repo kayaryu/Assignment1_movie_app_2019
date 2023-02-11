e15f2810f2776dac986422f528fa08dc096cd34c
kaya.ryu <kaya.ryu@asurion.com>
2023-02-11 18:48:25 +0900

    4-0 Fetching from API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movie: [],
   };
+
+  getMovies = async () => {
+    const movies = await axios.get(
+      "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
+    );
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 1000);
+    this.getMovies();
   }
+  // async componentDidMount() {
+  //   const movies = axios.get(
+  //     "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
+  //   );
+  // }
 
   render() {
     const { isLoading } = this.state;
