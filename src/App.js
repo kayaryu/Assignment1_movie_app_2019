27f900c7c7df37f87697b2f81e3203f0962a6456
kaya.ryu <kaya.ryu@asurion.com>
2023-02-11 15:48:11 +0900

    2-3 map Recap

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function Food({ name, picture }) {
       <img
         src={picture}
         width="240"
+        alt={name}
       />
     </div>
   );
@@ -14,34 +15,49 @@ function Food({ name, picture }) {
 
 const foodIlike = [
   {
+    id: 1,
     name: "wine",
     image:
       "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
   },
   {
+    id: 2,
     name: "potato",
     image:
       "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
   },
   {
+    id: 3,
     name: "tomato",
     image:
       "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
   },
   {
+    id: 4,
     name: "sth",
     image:
       "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
   },
 ];
 
+function renderFood(dish) {
+  console.log(dish);
+  return (
+    <Food
+      name={dish.name}
+      picture={dish.image}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       {foodIlike.map((dish) => (
         <Food
+          key={dish.id}
           name={dish.name}
-          picture={dish.image}
+          picture={dish.picture}
         />
       ))}
     </div>
