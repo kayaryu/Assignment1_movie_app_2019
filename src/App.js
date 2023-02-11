bc5acf0e4a7908961dbd7406c12af8ad69950cff
kaya.ryu <kaya.ryu@asurion.com>
2023-02-11 15:59:23 +0900

    2-4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
+const foodIlike = [
+  {
+    id: 1,
+    name: "wine",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+    rating: 4.8,
+  },
+  {
+    id: 2,
+    name: "potato",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+    rating: 5,
+  },
+  {
+    id: 3,
+    name: "tomato",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+    rating: 3,
+  },
+  {
+    id: 4,
+    name: "sth",
+    image:
+      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
+    rating: 4.7,
+  },
+];
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img
         src={picture}
         width="240"
@@ -13,42 +45,22 @@ function Food({ name, picture }) {
   );
 }
 
-const foodIlike = [
-  {
-    id: 1,
-    name: "wine",
-    image:
-      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
-  },
-  {
-    id: 2,
-    name: "potato",
-    image:
-      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
-  },
-  {
-    id: 3,
-    name: "tomato",
-    image:
-      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
-  },
-  {
-    id: 4,
-    name: "sth",
-    image:
-      "https://static01.nyt.com/images/2022/03/10/dining/10pour1/10pour1-superJumbo.jpg?quality=75&auto=webp",
-  },
-];
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
-function renderFood(dish) {
-  console.log(dish);
-  return (
-    <Food
-      name={dish.name}
-      picture={dish.image}
-    />
-  );
-}
+// function renderFood(dish) {
+//   console.log(dish);
+//   return (
+//     <Food
+//       name={dish.name}
+//       picture={dish.image}
+//     />
+//   );
+// }
+Food.propTypes = {};
 
 function App() {
   return (
@@ -57,7 +69,8 @@ function App() {
         <Food
           key={dish.id}
           name={dish.name}
-          picture={dish.picture}
+          picture={dish.image}
+          rating={dish.rating}
         />
       ))}
     </div>
